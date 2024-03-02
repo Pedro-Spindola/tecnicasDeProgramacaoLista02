1e7fba495a094989f27fcea0dc59c1b6ae9566a0
Pedro-Spindola <spindolapedrohenrique99@gmail.com>
2024-03-02 11:46:10 -0300

    Subindo a primeira versão para o git.

---
+++ src/main/java/tecnicasdeprogramacao/TecnicasDeProgramacao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tecnicasdeprogramacao;
+
+/**
+ *
+ * @author Pedro Spindola
+ */
+public class TecnicasDeProgramacao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
